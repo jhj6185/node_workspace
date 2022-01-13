7ead61950926fad0cf5c1ecded431314c270a746
jhj6185 <tkaqhcjstk+2@likelion.org>
2022-01-13 19:22:20 +0900

    Add : member.js랑 myServer.js는 수진님꺼 추가

---
+++ member_hj.js
@@ -0,0 +1,183 @@
+exports.listMember = function(){
+
+    router.route('/process/listMember').post(function(req,res){
+        console.log('/process/listMember 호출됨');
+        if(database){
+            // 1. 모든 사용자 검색
+            MemberModel.findAll(function(err,results){
+                if(err){
+                    console.error('사용자 리스트 조회 중 오류 발생 : '+err.stack);
+                    res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+                    res.write('<h2>사용자 리스트 조회 중 오류 발생</h2>');
+                    res.write('<p>'+err.stack+'</p>');
+                    res.end();
+                    return;
+                }
+                if(results){
+                    //결과 객체 있으면 리스트 전송 
+                    console.dir(results);
+                    res.writeHead('200',{'Content-Type': 'text/html;charset=utf8'});
+                    res.write('<h2>사용자 리스트</h2>');
+                    res.write('<div><ul>');
+                    for(var i=0; i<results.length; i++){
+                        var curUserId = results[i]._doc.userId; //여기다가 함수를 써줘야하네
+                        var curUserPwd = results[i]._doc.userPwd;//이거를 함수에 써서 함수만호출
+                        var curUserName = results[i]._doc.userName;
+                        var curUserAge = results[i]._doc.age;
+                        var curRegDate = results[i]._doc.regDate;
+                        var curUpdateDate = results[i]._doc.updateDate;
+                        res.write('<li>#'+i+' : '+curUserId+', '+curUserPwd+', '+
+                        curUserName+', '+curUserAge+', '+curRegDate+', '+curUpdateDate+', '+
+                        '</li>');
+                    }
+                    res.write('</ul></div>');
+                    res.end();
+                }else{
+                    //결과 객체 없으면 실패 응답 전송
+                    console.log("database on 아니라능");
+                }
+            }); //findAll end
+         }
+    })
+    
+}
+
+//===================================authMember()===================================
+
+
+exports.authMember = function(){
+
+    router.route('/process/login').post(function(req,res){
+
+        console.log('/process/login 호출됨');
+          //요청 파라미터 확인
+         var userId = req.body.userId || req.query.userId;
+         var userPwd = req.body.userPwd || req.query.userPwd;
+         
+         //데이터베이스 객체가 초기화된 경우, authMember 함수 호출하여 사용자 인증
+         if(database){
+             authMember_call(database,userId,userPwd, function(err, docs){
+                 if(err){
+                     throw err;
+                 }
+                 //조회된 레코드가 있으면 성공 응답 전송
+                 if(docs){
+                     res.writeHead('200',{'Content-Type': 'text/html;charset=utf8'});
+                     res.write('<h1>로그인 성공</h1>');
+                     res.end();
+                 } else{
+                     //조회된 레코드가 없는 경우 실패 응답 전송
+                     res.writeHead('200',{'Content-Type': 'text/html;charset=utf8'});
+                     res.write('<h1>로그인 실패</h1>');
+                     res.end();
+                 }
+             });
+         }else{
+             res.writeHead('200', { 'Content-Type':'text/html;charset=utf8'});
+             res.write('<h2>데이터베이스 연결 실패</h2>');
+             res.end();
+         }
+          
+      });
+    
+}
+
+var authMember_call = function(database, userId, userPwd, callback){
+    console.log('authMember 호출됨 : '+userId+','+userPwd);
+    // 1. 아이디를 사용해 검색
+    MemberModel.findById(userId, function(err,results){
+        if(err){
+            callback(err, null);
+            return;
+        }
+        console.log('아이디 [%s]로 사용자 검색결과',userId);
+        if(results.length>0){
+            console.log('아이디와 일치하는 사용자 찾음.');
+            //callback(null,null);
+            // 2. 비밀번호 확인
+            if (results[0]._doc.userPwd== userPwd){
+                console.log('비밀번호 일치함');
+                callback(null,results);
+            }
+        }else{
+            console.log("아이디와 일치하는 사용자를 찾지 못함");
+            callback(null,null);
+        }
+    });
+}
+//===================================addMember()==========================================
+exports.addMember = function(){
+
+    router.route('/process/addMember').post(function(req,res){
+    
+        console.log('/process/addMember 호출됨');
+          //요청 파라미터 확인
+         var userId = req.body.userId || req.query.userId;
+         var userPwd = req.body.userPwd || req.query.userPwd;
+        var userName= req.body.userName || req.query.userName;
+        var age = req.body.age || req.query.age;
+    
+         console.log('요청 파라미터 : '+userId+','+userPwd+','+userName+','+age);
+         //데이터베이스 객체가 초기화된 경우, authMember 함수 호출하여 사용자 인증
+         if(database){
+             addMember_call(database,userId,userPwd,userName,age, function(err, addedUser){ //result는 
+                //addMember에 올라가면 result를 callback(null, result);로 해주고있어서
+                // 그 result가 여기의 result인데 어떤 데이터를 insert하게되면,
+                //result객체에 몇개의 데이터가 삽입됐는지 뜨고, 어떤 데이터가 삽입되었는지 
+                //result에 대한 정보가 쫙 들어있는데 그거가 있고, insertedCount가 0보다 큰지
+                //이런거로 확인해주는 것...(근데 playground result에는 result의 아이디값만 뜸..)
+                 if(err){
+                     throw err;
+                 }
+                 //추가된 데이터가 있으면 성공 응답 전송
+                 if(addedUser){
+                     console.dir(addedUser);
+                     res.writeHead('200',{'Content-Type': 'text/html;charset=utf8'});
+                     res.write('<h1>회원가입 성공</h1>');
+                     res.end();
+                 } else{
+                     //결과 객체가 없으면 실패 응답 전송
+                     res.writeHead('200',{'Content-Type': 'text/html;charset=utf8'});
+                     res.write('<h1>회원가입 실패</h1>');
+                     res.end();
+                 }
+             });
+         }else{ //데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+             res.writeHead('200', { 'Content-Type':'text/html;charset=utf8'});
+             res.write('<h2>데이터베이스 연결 실패</h2>');
+             res.end();
+         }
+          
+      });
+    
+}
+
+var addMember_call = function(database, userId, userPwd,userName,age, callback){
+    console.log('addMember 호출 : '+userId+","+userPwd+","+userName+","+age);
+    //MemberModel 인슨턴스 생성
+    var user= new MemberModel({"userId": userId, "userPwd": userPwd, "userName": userName, 
+    "age": age});
+    // save()로 저장 : 저장 성공시 addedUser 객체가 파라미터로 전달됨
+    user.save(function(err, addedUser){
+        console.log("addedUser%j",addedUser);
+        if(err){
+            callback(err,null);
+            return;
+        }
+        console.log("사용자 데이터 추가함");
+        callback(null, addedUser);
+    });
+}
+//==========================================================================================
+
+app.use('/', router) //라우터 객체를 app객체에 등록
+
+app.all('*', function(req, res){//등록되지 않은 패스에 대해 페이지 오류 응답
+    res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>');
+})
+
+app.listen(app.get('port'), function(){
+    console.log('서버가 시작되었습니다. 포트 : '+app.get('port'));
+    //데이터베이스 연결을 위한 함수 호출
+    connectDB();
+});
--- myServer.js
+++ myServer.js
@@ -1,89 +1,205 @@
-//express 기본 모듈 불러오기
-var express = require('express'), http=require('http'), path=
-require('path');
-//express의 미들웨어 불러오기
-let bodyParser = require('body-parser'), 
-cookieParser = require('cookie-parser'),
-static = require('serve-static'),
-errorHandler = require('errorhandler');
-var mongoose = require('mongoose');
-//에러 핸들러 모듈 사용
+let express = require('express'),
+    http = require('http'),
+    path = require('path');
+//Express 미들웨어 불러오기
+let bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    static = require('serve-static'),
+    errorHandler = require('errorhandler');
+//오류 핸들러 모듈 사용
 let expressErrorHandler = require('express-error-handler');
 //Session 미들웨어 불러오기
-var expressSession = require('express-session');
+let expressSession = require('express-session')
+//몽고디비 모듈 사용
+let MongoClient = require('mongodb').MongoClient;
+//몽구스 모듈 사용
+var mongoose = require('mongoose');
+
+// 데이터베이스 객체를 위한 변수 선언
+let database;
+ 
+// 데이터베이스 스키마 객체를 위한 변수 선언
+let MemberSchema;
+ 
+// 데이터베이스 모델 객체를 위한 변수 선언
+let MemberModel;
 //익스프레스 객체 생성
 let app = express();
-//mongodb 모듈 사용
-let MongoClient = require('mongodb').MongoClient;
-const { MongoKerberosError } = require('mongodb');
-
-//데이터베이스 객체를 위한 변수 선언
-let database;
-let MemberSchema;
-let MemberModel;
-
-//데이터베이스에 연결
-function connectDB(){
-    // 데이터 베이스 연결 정보
-    let databaseUrl = 'mongodb://localhost:27017/bitDB';
-    //데이터베이스 연결
-        console.log('데이터베이스에 연결을 시도합니다.');
-        mongoose.Promise = global.Promise; //mongoose의 promise 객체는 
-        //global의 promise객체 사용하도록 함
-        mongoose.connect(databaseUrl);
-        database=mongoose.connection;
-        database.on('error',console.error.bind(console, 'mongoose connection error.'));  
-        
-        database.on('open', function(){
-            console.log('데이터베이스에 연결되었습니다. : '+databaseUrl);
-            //스키마 정의
-            MemberSchema = mongoose.Schema({
-                userId:{ type : String, required: true, unique: true},
-                userPwd: { type : String, required: true },
-                userName: { type : String, index: 'hashed' },
-                age: {type:Number, 'default' : -1},
-                // age: {type:Number},
-                regDate : { type: Date, index : { unique : false }, 'default': Date.now},
-                updateDate : { type: Date, index: { unique : false }, 'default': Date.now}
-            });
-
-            //스키마에 static 메소드 추가
-            MemberSchema.static('findById', function(userId, callback){
-                return this.find({ userId : userId }, callback);
-            });
-            MemberSchema.static('findAll', function(callback){
-                return this.find({}, callback);
-            });
-
-            console.log('MemberSchema 정의함');
-            //MemberModel 모델 정의
-            MemberModel = mongoose.model("members2", MemberSchema); //members2라는 컬렉션이
-            //db에있다면 그것을 그대로 사용하기 위해 짝을 지어주고, 없으면 만들어서 사용해주는 기능
-            console.log('MemberModel 정의함');
-        });
-        //연결 끊어졌을 때 5초후 재연결
-        database.on('disconnected',function(){
-            console.log('연결이 끊어졌습니다. 5초후 재연결합니다.');
-            sestInterval(connectDB, 5000);
-        });
-}
-
-//기본 속성 설정
 app.set('port', process.env.PORT || 3000);
-//body-parser를 이용해 application/x-www-form-urlencoded 파싱
-app.use(bodyParser.urlencoded({extended: false}))
-//body-parser를 이용해 application/json 파싱
-app.use(bodyParser.json())
-//public폴더를 static으로 오픈
-app.use('/public',static(path.join(__dirname,'public')));//public 폴더를 /public이라는 주소창의 경로로 무조건 받겠다
-//이런 뜻이랭..
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use('/public', static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'my key', resave: true, saveUninitialized: true
+}));
+// 데이터베이스에 연결
+function connectDB(){
+    // 데이터베이스 연결 정보
+    var databaseUrl = 'mongodb://localhost:27017/bitDB';
+    
+    // 데이터베이스에 연결
+    console.log('데이터베이스 연결을 시도합니다.');
+    mongoose.Promise = global.Promise; //mongoose의 Promise객체는 global의 Promise객체 사용하도록 함
+    mongoose.connect(databaseUrl);
+    database = mongoose.connection;
+    
+    database.on('error', console.error.bind(console, 'mongoose connection error'));
+    database.on('open', function(){
+        console.log('데이터베이스에 연결되었습니다. : ' + databaseUrl);
+         //스키마 정의
+         var MemberSchema = new mongoose.Schema({
+            userId : {type: String , required:true, unique:true},
+            userPwd :  {type: String , required:true},
+            userName: {type: String, index:'hashed'},
+            age: {type: Number,'default':-1},
+            regDate: {type: Date, index:{unique:false},'default':Date.now},
+            updateDate: {type: Date, index:{unique:false},'default':Date.now}
+       });
+        
+       //스키마에 static 메소드 추가
+        MemberSchema.static('findById', function(userId, callback){
+            return this.find({userId:userId}, callback);
+        });
+        //모든 문서 데이터 반환
+        MemberSchema.static('findAll', function(callback){
+            return this.find({}, callback);
+        });
+        
+        console.log('스키마가 정의됨' );
+        
+        //MemberModel 정의
+        MemberModel = mongoose.model("members2", MemberSchema);
+        console.log('MemberModel 정의함.');
+    });
+    
+    // 연결 끊어졌을 때 5초 후 재연결
+    database.on('disconnected', function(){
+        console.log('연결이 끊어졌습니다. 5초 후 다시 연결합니다.');
+        setInterval(connectDB, 5000);
+    });
+}
+//라우팅 함수 등록
 var router = express.Router();
+var member=require('./member');//member 객체 불러오기
+var listMember = member.listMember; //member객체에있는 함수 불러오기
+var addMember = member.addMember;
+var authMember = member.authMember;
 
-var listMember = require('./member').listMember;
-var authMember = require('./member').authMember;
-var addMember = require('./member').addMember;
+//회원 리스트 조회
+router.route('/process/listMember').post(function(req, res){ //이 경로로 들어오면
+    console.log('/process/listMember 호출됨');
+    
+    if(database){
+        MemberModel.findAll(function(err, results){
+            if(err){
+                console.log('사용자 리스트 조회중 에러 발생 : '+err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h1> 사용자 리스트 조회중 오류 발생 </h1>');
+                res.write('<p>' + err.stack + '</p');
+                res.end();
+                return;
+            }
+            
+            if(results.length>0){ //결과 객체 있으면 리스트 전송 
+                console.dir(results);
+                listMember(results,res);    
+               
+            }else{
+            console.log('db 연결 오류 ');
+            res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+            res.write('<h1> DB 연결 실패</h1>');
+            res.end();
+            }
+        });
+    }
+});
+//사용자 추가 라우팅 함수 
+router.route('/process/addMember').post(function(req,res){
+    console.log('/process/addMember 호출됨');
+    
+    var userId = req.body.userId || req.query.userId;
+    var userPwd = req.body.userPwd || req.query.userPwd;
+    var userName = req.body.userName || req.query.userName;
+    var age = req.body.age || req.query.age;
+    
+    console.log('요청 파라미터 : ' + userId + ', ' + userPwd + ', ' + userName +',' + age);
+    
+    // 데이터베이스 객체가 초기화된 경우, addMember 함수 호출하여 사용자 추가
+    if (database){
+        var user = new MemberModel({"userId":userId, "userPwd":userPwd, "userName":userName,"age":age});
+        addMember(database,user,userId, userPwd, userName, age,function(err, addedUser){
+            if(err) {throw err;}
+            
+            //결과 객체 확인하여 추가된 데이터 있으면 성공 응답 전송
+            if(addedUser){
+                console.dir(addedUser);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>사용자 추가 성공</h2>');
+                res.end();
+            }else{
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>사용자 추가 실패</h2>');
+                res.end();
+            }
+        });
+    }else{
+        // 데이터베이스 객체가 초기화되지 않는 경우 실패응답 전송
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.end();
+    }
+});
 
-listMember(app);
-authMember(app);
-addMember(app);
+router.route('/process/login').post(function (req, res) {
+    console.log('process/login 호출됨');
+    //요청 파라미터 확인
+    var userId = req.body.userId || req.query.userId;
+    var userPwd = req.body.userPwd || req.query.userPwd;
+    console.log('요청 파라미터 : ' + userId + ', ' + userPwd);
+    //데이터베이스 객체가 초기화된 경우, authMember함수 호출하여 사용자 인증
+    if (database) {
+        authMember(database, MemberModel,userId, userPwd,
+            function (err, docs) {
+                if (err) {throw err;}
+                //조회된 레코드가 있으면 성공 응답 전송   
+                if (docs) {
+                    res.writeHead(200, {
+                        "Content-Type": "text/html;charset=utf8"
+                    });
+                    res.write('<h1>로그인 성공</h1>');
+                    res.end();
+                } else {//조회된 레코드가 없는 경우 실패 응답 전송
+                    res.writeHead(200, {
+                        "Content-Type": "text/html;charset=utf8"
+                    });
+                    res.write('<h1>로그인 실패</h1>');
+                    res.end();
+                }
+            });
+    } else {
+        res.writeHead(200, {
+            "Content-Type": "text/html;charset=utf8"
+        });
+        res.write('<h1>데이터베이스 연결 실패</h1>');
+        res.end();
+    }
+}
+);
+        
+app.use('/', router);
+
+
+//404 Error Handling
+errorHandler = expressErrorHandler({
+    static: { '404': './public/404.html'}
+});
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+//서버 시작
+app.listen(app.get('port'), function(){
+    console.log('서버가 시작되었습니다. 포트' + app.get('port'));
+    connectDB(); //데이터베이스 연결
+});
